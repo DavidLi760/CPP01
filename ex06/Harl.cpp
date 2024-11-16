e2789e5e0d335bc036abb0e38ba18479f27e5776
David Li <davli@made-f0Ar7s11.clusters.42paris.fr>
2024-11-16 15:12:45 +0100

    s

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -12,54 +12,20 @@
 
 #include "Harl.hpp"
 
-void    Harl::debug(void){
-    std::cout << "Harl : I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-}
-
-void    Harl::info(void){
-    std::cout << "Harl : I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-}
-
-void    Harl::warning(void){
-    std::cout << "Harl : I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
-}
-
-void    Harl::error(void){
-    std::cout << "Harl : This is unacceptable! I want to speak to the manager now." << std::endl;
-}
-
-void    Harl::complain(std::string level) {
-    void (Harl::*functions[4])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
-        std::string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-
-    for (int i = 0; i < 4; i++) {
-        if (level == levels[i]) {
-            (this->*functions[i])();
-            return;
-        }
-    }
-    std::cout << "Harl : ....." << std::endl;
-}
-
 void    filtering(int level){
-    int i = level;
-    while ()
     switch (level)
     {
-    case 4:
-        std::cout << "[ ERROR ]\nThis is unacceptable, I want to speak to the manager now." << std::endl;
-        break;    
-    case 3:
-        std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free.\nI've been coming for years whereas you started working here since last month." << std::endl;
-        break;
-    case 2:
-        std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
-        break;
-    case 1:
-        std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-        break;
-    default:
-        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-        break;
+        case 1:
+            std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n" << std::endl;
+        case 2:
+            std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n" << std::endl;
+        case 3:
+            std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free.\nI've been coming for years whereas you started working here since last month.\n" << std::endl;
+        case 4:
+            std::cout << "[ ERROR ]\nThis is unacceptable, I want to speak to the manager now.\n" << std::endl;
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            break;
     }
 }
--- ex06/Harl.hpp
+++ ex06/Harl.hpp
@@ -15,17 +15,6 @@
 
 # include <iostream>
 # include <string>
-# include <map>
-
-class Harl{
-    private:
-        void    debug();
-        void    info();
-        void    warning();
-        void    error();
-    public:
-        void    complain(std::string level);
-};
 
 void    filtering(int level);
 
